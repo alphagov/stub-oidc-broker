9432b4ad69f1289b46ad0c93eace5f1cabc4d45c
James Howes <james.howes@digital.cabinet-office.gov.uk>
2019-10-16 16:31:43 +0100

    Start Redis when starting application
    - This is so the stub-client can now store items such as the state or nonce, which
    it will use to validate incoming responses

--- startup.sh
+++ startup.sh
@@ -7,4 +7,7 @@ cd "$(dirname "$0")"
 
 ./gradlew installDist
 
+trap "docker container stop clientRedis" EXIT
+docker run --name clientRedis -d -p 6380:6379 --rm redis
+
 ./build/install/verify-stub-client/bin/verify-stub-client server $CONFIG_FILE
