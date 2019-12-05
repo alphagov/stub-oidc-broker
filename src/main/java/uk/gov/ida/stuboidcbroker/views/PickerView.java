e4d5a5e213377ba1fe77cb2ae1a8cb08e5cca02e
brenetic <b@brenetic.com>
2019-12-05 15:30:03 +0000

    Use different CSS for picker page
    We want it to represent a private broker.

--- src/main/java/uk/gov/ida/stuboidcbroker/views/PickerView.java
+++ src/main/java/uk/gov/ida/stuboidcbroker/views/PickerView.java
@@ -6,14 +6,21 @@ import uk.gov.ida.stuboidcbroker.domain.Organisation;
 import java.util.List;
 
 public class PickerView extends View {
+    private int index;
     private List<Organisation> orgList;
 
     public PickerView(List<Organisation> orgList) {
         super("picker.mustache");
         this.orgList = orgList;
+        this.index = 0;
     }
 
     public List<Organisation> getOrgList() {
         return orgList;
+    }
+
+    public int getIndex() {
+        this.index = ++index;
+        return index;
     }
 }
