2ecf0cc1561a1b44f81dc317968583700a7b799c
James Howes <james.howes@digital.cabinet-office.gov.uk>
2020-01-07 10:01:55 +0000

    Make the picker page dynamic
    - Change the picker page so that it can handle multiple custom brokers. Previously
    if there were multiple custom brokers to choose from, then the template would break. This change
    caters for multiple custom brokers to select from.

--- src/main/java/uk/gov/ida/stuboidcbroker/views/PickerView.java
+++ src/main/java/uk/gov/ida/stuboidcbroker/views/PickerView.java
@@ -3,7 +3,10 @@ package uk.gov.ida.stuboidcbroker.views;
 import io.dropwizard.views.View;
 import uk.gov.ida.stuboidcbroker.domain.Organisation;
 
+import java.util.ArrayList;
 import java.util.List;
+
+import static java.lang.String.valueOf;
 
 public class PickerView extends View {
     private int index;
@@ -50,5 +53,18 @@ public class PickerView extends View {
 
     public String getSchemeLogoClassName() {
         return scheme.equals("1") ? "scheme-2" : "scheme-1";
+    }
+
+    public List<String> getDummyBrokers() {
+        int listSize = 8 - brokers.size();
+        List<String> dummyList = new ArrayList<>();
+        for (int j = 0; j < listSize; j++) {
+            dummyList.add(valueOf(j));
+        }
+        return dummyList;
+    }
+
+    public boolean startNewRow() {
+        return index % 2 == 0;
     }
 }
